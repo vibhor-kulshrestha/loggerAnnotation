cbb51bf1b549437414b2723f7b41d5b43a71b7b4
vibhorkulshrestha-mobikwik <vibhor.kulshrestha@mobikwik.com>
2026-08-16 18:44:05 +0530

    Add root Gradle skeleton and wrapper for multi-project build.
    Sets up version catalog, plugin management, and Gradle 8.13 wrapper for annotations, runtime, compiler-plugin, and sample-android modules.

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "loggerAnnotation"
+
+include(":annotations")
+include(":runtime")
+include(":compiler-plugin")
+include(":sample-android")
